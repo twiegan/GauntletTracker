76192fa1824e52e2bc098bbad50f22f1f71142c0
twiegan <twiegan@purdue.edu>
2021-11-15 16:41:13 -0500

    Firebase config setup

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+// Firebase Configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCTlvBM05NCb1UqzSOtyxRZmEOi2D_V_kk",
+  authDomain: "gauntlettracker.firebaseapp.com",
+  projectId: "gauntlettracker",
+  storageBucket: "gauntlettracker.appspot.com",
+  messagingSenderId: "558145500004",
+  appId: "1:558145500004:web:b6ef830e9576e2cf95c071"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
